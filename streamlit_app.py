e1c6c50b01b4a84954c7d008c1b4bf09f9798234
Santhu2206 <139215840+Santhu2206@users.noreply.github.com>
2023-07-17 23:17:34 +0530

    Update streamlit_app.py
    try if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,24 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 streamlit.write('The user entered ', fruit_choice)
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information.")
+  else 
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+  # take the json version of the response and normalzie it
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # output the screen as table 
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-# streamlit.text(fruityvice_response.json()) # just write the data to the screen
 
-# take the json version of the response and normalzie it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output the screen as table 
-streamlit.dataframe(fruityvice_normalized)
+# streamlit.text(fruityvice_response.json()) # just write the data to the screen
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
